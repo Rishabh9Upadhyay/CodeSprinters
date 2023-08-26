d84560b584a660eb03460cba24969dce5d462a70
Rishabh Upadhyay <103876066+Rishabh9Upadhyay@users.noreply.github.com>
2023-08-27 00:06:13 +0530

    Add files via upload

---
+++ src/models/contact.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const contactSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : Number,
+        required : true
+    },
+    textsms : {
+        type : String,
+        maxlength: 500,
+        required : true
+    },
+    date : {
+        type : Date,
+        default : Date.now()
+    }
+})
+
+const massage = new mongoose.model("Massage",contactSchema);
+
+module.exports = massage;
